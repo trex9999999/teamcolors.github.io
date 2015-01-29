050420e81f3bf22be6ff961c5a8154f7e621fac2
Jim Nielsen <jimniels@gmail.com>
2015-01-29 12:29:47 -0700

    more gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var path            = require('path');
 var notify          = require('gulp-notify');
 var plumber         = require('gulp-plumber');
 //var del             = require('del');
+var Handlebars      = require('handlebars');
+var handlebarsCompile      = require('gulp-compile-handlebars');
 
 var INPUT = 'src';
 var OUTPUT = 'public';
@@ -61,22 +63,56 @@ gulp.task('sass', function() {
 //         .pipe(gulp.dest('dist'));
 // });
 
+// gulp.task('handlebars', function(){
+//     var source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
+//              "{{kids.length}} kids:</p>" +
+//              "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+//     //var source = require('../src/html/tmpl.hb');
+//     var template = handlebars.compile(source);
+     
+//     var data = { "name": "Alan", "hometown": "Somewhere, TX",
+//                  "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+//     var result = template(data);
+//     console.log(result);
+//     return gulp
+//       .src(path.join(INPUT, 'html/tmpl.hb'))
+//       .pipe()
+//       .pipe(gulp.dest(path.join(OUTPUT, 'html')));
+// });
+
+
+
+// Handlebars
+gulp.task('handlebars', function(){
+    var templateData = {leagues: require('./src/data/team-data.json')};
+    return gulp
+        .src(path.join(INPUT, 'html/tmpl.hbs'))
+        .pipe(handlebarsCompile(templateData))
+        //.pipe(rename('hello.html'))
+        .pipe(gulp.dest(path.join(OUTPUT, 'html')));
+});
+
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch(path.join(INPUT, 'styles/*.scss'), ['sass']);
+    gulp.watch(path.join(INPUT, 'html/*.hb'), ['handlebars']);
 });
 
 // Default Task
-gulp.task('default', ['sass','watch']);
-// gulp.task('default', ['clean'], function() {
-//     gulp.start('sass', 'watch');
-// });
+gulp.task('default', ['sass','watch', 'handlebars']);
 
-// gulp.task('dev', ['clean'], function() {
-//   gulp.start(
-//     'sass',
-//     'watch'
-//   );
-// });
 
-// gulp.task('default', ['dev']);
+// Handlebars helpers
+Handlebars.registerHelper('toLowerCase', function(str) {
+  return str.toLowerCase();
+});
+
+Handlebars.registerHelper('hex2rgb', function(hex){
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    var rgb = {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    };
+    return 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
+});
